3fef111c1be422a68bc4fb89ac652f49d5a73230
SevOsmanko <s.osmanko-12@student.lboro.ac.uk>
2014-04-30 09:05:11 +0100

    Bare - No functionality

---
+++ Team08ASP/Models/Modules.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team08ASP.Models
+{
+    public class Modules
+    {
+    }
+}
---
+++ Team08ASP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team08ASP.Startup))]
+namespace Team08ASP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
