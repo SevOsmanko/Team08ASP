39e6c719980533e9f22deed1fc95ef00586f8c27
SevOsmanko <s.osmanko-12@student.lboro.ac.uk>
2014-05-06 13:58:28 +0100

    Bare - New

--- Team08ASP/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Team08ASP.MvcApplication" Language="C#" %>
---
+++ TeamASP/Controllers/ModulesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeamASP.Controllers
+{
+    public class ModulesController : Controller
+    {
+        //
+        // GET: /Modules/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
